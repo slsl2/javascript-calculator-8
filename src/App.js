f9a43a9839ed31429e04425582326c307ca5fff8
slsl2 <kimminji7153@gmail.com>
2025-10-15 20:13:22 +0900

    feat(app): add basic sum function using , :

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
+    const arr = str.split(/[,:]/);
+    let sum = 0;
+    arr.forEach((num) => {
+      sum += Number(num);
+    });
+    Console.print(`결과 : ${sum}`);
+  }
 }
 
 export default App;
