89e1b71e155a77db837c689de3c7995a3f304191
slsl2 <kimminji7153@gmail.com>
2025-10-17 02:07:26 +0900

    feat(app): add custom delimiter logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { Console } from "@woowacourse/mission-utils";
 
+// 예외처리
+//
+
 class App {
   async run() {
+    // 문자열 입력
     const str = await Console.readLineAsync("덧셈할 문자열을 입력해 주세요.\n");
-    const arr = str.split(/[,:]/);
+
+    let numbers = [];
+    let custom = str.match(/^\/\/(.)\\n(.*)$/); // 커스텀 구분자가 있는지 확인
+    if (custom) {
+      const delimiter = custom[1];
+      numbers = custom[2].split(delimiter);
+    } else {
+      numbers = str.split(/[,:]/); // 커스텀 구분자가 없을 경우에는 쉼표와 콜론을 구분자로
+    }
+
+    // 덧셈
     let sum = 0;
-    arr.forEach((num) => {
-      sum += Number(num);
+    numbers.forEach((num) => {
+      sum += Number(num); // 요소 하나씩 돌면서 덧셈
     });
     Console.print(`결과 : ${sum}`);
   }
